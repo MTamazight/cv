d057390937ab63bd85717b8bff00820eee3b6f83
MTamazight <46898577+MTamazight@users.noreply.github.com>
2019-02-05 20:30:19 +0100

    Update TPHTML.js

--- TPHTML.js
+++ TPHTML.js
@@ -1,4 +1,5 @@
 window.addEventListener("scroll", function() { 
-let deplacement = ((window.scrollY)*100)/(document.body.offsetHeight-window.innerHeight);
+//let deplacement = ((window.scrollY)*100)/(document.body.offsetHeight-window.innerHeight);
+  window.scrollTo(300, 500);
 document.getElementById('progress').value = deplacement; 
 });
