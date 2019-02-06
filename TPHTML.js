2ebbba872b10f638fab641a56a908486da0f121e
MTamazight <46898577+MTamazight@users.noreply.github.com>
2019-02-06 19:11:23 +0100

    Update TPHTML.js

--- TPHTML.js
+++ TPHTML.js
@@ -1,14 +1,14 @@
-/*window.addEventListener("scroll", function() { 
+window.addEventListener("scroll", function() { 
   window.scrollY // Valeur du scroll en pixel
 document.body.offsetHeight // Hauteur totale de la page en pixel
 window.innerHeight // Hauteur de l'écran en pixel
 let deplacement = ((window.scrollY)*100)/(document.body.offsetHeight-window.innerHeight);
   window.scrollTo(50, 100);
 document.getElementById('progress').value = deplacement; 
-});*/
+});
 
 
-window.addEventListener("scroll", function() { 
+/*window.addEventListener("scroll", function() { 
 
 // Calculez le déplacement ici ...
 var deplacement=(window.scrollY+window.innerHeight)*15/document.body.offsetHeight;
@@ -21,5 +21,5 @@ function myScroll() {
     
     
     window.scroll(0,progress) ;
-}
+}*/
 
