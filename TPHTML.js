5eca5d720c72286d86da2cfd6e72715dc21ade53
MTamazight <46898577+MTamazight@users.noreply.github.com>
2019-02-06 22:27:53 +0100

    Update TPHTML.js

--- TPHTML.js
+++ TPHTML.js
@@ -1,11 +1,9 @@
-
-
-$(document).ready(function() {
-                $('nav a').on('click', function() { // Au clic sur un élément
-                        var page = $(this).attr('href'); // Page cible
-                        var speed = 750; // Durée de l'animation (en ms)
-                        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-                        return false;
-                });
-        });
-
+window.addEventListener("scroll", function() { 
+  var deplacement=(window.scrollY+window.innerHeight)*15/document.body.offsetHeight;
+document.getElementById('progress_bar').value = deplacement; 
+});
+function myScroll() { 
+    var progress = document.getElementById('progress_bar').value; 
+    progress=progress*document.body.offsetHeight/15-window.innerHeight;
+    window.scroll(0,progress) ;
+}
